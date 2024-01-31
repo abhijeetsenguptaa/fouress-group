bf7391d25f4f3fd72981eb7e3d8aea98d7aed173
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2024-01-31 17:34:13 +0530

    updated otp verification for email

--- services/users/phone-number-register-login.js
+++ services/users/phone-number-register-login.js
@@ -64,11 +64,11 @@ async function phoneNumberLoginService(phoneNumber, role) {
         //     .then(json => console.log(json))
         //     .catch(err => console.error('error:' + err));
         // Send OTP via Twilio
-        // await twilioClient.messages.create({
-        //     from: '+13237035099',
-        //     to: '+91'+ phoneNumber,
-        //     body: `Your OTP is: ${otp}`
-        // });
+        await twilioClient.messages.create({
+            from: '+13237035099',
+            to: '+91'+ phoneNumber,
+            body: `Your OTP is: ${otp}`
+        });
 
         // Hash OTP before saving to the database
         const hashOTP = await bcrypt.hash(otp, 10);
